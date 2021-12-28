188a3b632e6c41fffe91b9bc2f34125a770f3c00
Craig Cravath <craig.cravath@willowtreeapps.com>
2021-12-28 15:45:20 -0500

    add style to ExpenseItem component

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 const ExpenseItem = () => {
   return (
-    <div>
+    <div className="expense-item">
       <div>Date</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Title</h2>
-        <div>Amount</div>
+        <div className="expense-item__price">Amount</div>
       </div>
     </div>
   );
