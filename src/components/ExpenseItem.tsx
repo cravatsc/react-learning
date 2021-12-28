3d8cda8826d967794cc017a7db31c956a0effc80
Craig Cravath <craig.cravath@willowtreeapps.com>
2021-12-28 16:24:54 -0500

    deconstruct component props

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -7,13 +7,13 @@ export interface Expense {
   date: Date;
 }
 
-export const ExpenseItem: React.FC<Expense> = (expense) => {
+export const ExpenseItem: React.FC<Expense> = ({ title, date, amount }) => {
   return (
     <div className="expense-item">
-      <div>{expense.date.toDateString()}</div>
+      <div>{date.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>{expense.title}</h2>
-        <div className="expense-item__price">{expense.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
       </div>
     </div>
   );
